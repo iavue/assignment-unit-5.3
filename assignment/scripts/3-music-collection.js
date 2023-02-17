20714235a215e933dbcf456e3e46898556a083c7
Ia Vue <iavue00@gmail.com>
2023-02-16 20:18:41 -0600

    making progress on showCollection function

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -22,7 +22,6 @@ console.log( addToCollection('Be', 'BTS', 2020) );
 // Console.log each album as added using the returned value.
 // After all are added, console.log the collection array.
 
-
 console.log( addToCollection('Proof', 'BTS', 2022) ); // added albums
 console.log( addToCollection('Jack in the Box', 'J-Hope', 2022) );
 console.log( addToCollection('Indigo', 'RM', 2022) );
@@ -31,3 +30,29 @@ console.log( addToCollection('thank u, next', 'Ariana Grande', 2019) );
 console.log( addToCollection('Justice', 'Justin Bieber', 2021) );
 console.log('Collection:', collection); // testing collection array to see the album
 
+// Add a function named showCollection. This function should:
+// Take in an array parameter. (This allows it to be reused to show any collection, like the results from the find or search.)
+// Console.log the number of items in the array.
+// Loop over the array and console.log each album's information formatted like: TITLE by ARTIST, published in YEAR.
+
+
+function showCollection( anArray ){ // created showCollection function with a parameter as anArray because we want an array there 
+    console.log('Length:', anArray.length);
+    for (let i=0; i<anArray.length; i++){
+        console.log(anArray.title, 'by', anArray.artist, ',', ' published in ', anArray.year);
+    }
+} // end showCollection
+
+// Test the showCollection function.
+
+let secondCollection = [  // creating a new collection here to test the showCollection function with a different collection
+    {title: 'Map of the Soul: 7', artist: 'BTS', year: 2020},
+    {title: '30', artist: 'Adele', year: 2021},
+    {title: 'Traumatize', artist: 'Megan Thee Stallion', year: 2022} ] // end secondCollection
+
+showCollection( secondCollection );
+
+//
+//
+//
+//
